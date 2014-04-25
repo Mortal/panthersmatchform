e88605cf6b3b80c65476d44b1843f5d6bd129f49
Michael Sondergaard <msond@cs.au.dk>
2014-04-25 23:33:51 +0200

    Add remaining views

--- app.js
+++ app.js
@@ -5,48 +5,76 @@ var App = function(elm) {
 };
 
 App.prototype.Start = function() {
-  var set = new App.SetModel();
-  this.setView = new this.SetView({model: set});
-  $(this.mainElement).append(this.setView.render().el);
+  this.appView = new App.AppView();
+  $(this.mainElement).html(this.appView.render().el);
 };
 
-App.SetModel = Backbone.Model.extend({
+App.Set = Backbone.Model.extend({
   defaults: function() {
     return {
       number: 1,
-      teams: ["ASV 7", "Viborg"]
+      teams: ["ASV 7",
+              "Viborg"]
     }
   }
 });
-
 
 App.AppView = Backbone.View.extend({
   tagName: "div",
   className: "screen",
   initialize: function() {
-    this.setModel = new App.SetModel();
-    this.setView = new this.SetView({model: set});
+    this.activeSet = new App.Set();
+  },
+  render: function() {
+    this.setView = new App.SetView({model: this.activeSet});
+    this.modalView = new App.ModalView();
+    this.$el.append(this.setView.render().el);
+    this.$el.append(this.modalView.render().el);
+    return this;
   }
 });
+
+App.TeamViewModel = Backbone.Model.extend();
 
 App.SetView = Backbone.View.extend({
   tagName: "div",
   className: "set",
   initialize: function() {
+    this.template = _.template($('#set_main').html());
   },
   render: function() {
-    this.$el.html();
+    this.$el.html(this.template({
+      number: this.model.get('number')
+    }));
+    this.leftTeamView = new App.TeamView({
+                              el: this.$('.team_left')
+                        }).render();
+    this.rightTeamView = new App.TeamView({
+                              el: this.$('.team_right')
+                        }).render();
     return this;
   }
 });
 
-
-App.ScoreView = Backbone.View.extend({
-  tagName: "div",
-  className: "set_score",
-  events: { },
+App.TeamView = Backbone.View.extend({
   initialize: function() {
+    this.template = _.template($('#team_main').html());
   },
   render: function() {
+    this.$el.html(this.template({score: 0,
+                                 name: "derp"}));
+    return this;
+  }
+});
+
+App.ModalView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'modal',
+  initialize: function() {
+    this.template = _.template($('#modal_main').html());
+  },
+  render: function() {
+    this.$el.html(this.template());
+    return this;
   }
 });
