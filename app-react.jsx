ae6c57ac7c528e367fd735575f9a538e325b0c4c
Mathias Rav <rav@cs.au.dk>
2014-04-27 10:28:09 +0200

    Factor out TeamSet

--- app-react.jsx
+++ app-react.jsx
@@ -10,89 +10,9 @@ var MatchForm = React.createClass({
 
     <div className="set_header">Sæt 3</div>
 
-    <div className="set_team team_left">
-      <div className="set_score">
-        <div className="set_team_name">ASV 7</div>
-        <button className="set_score_button">
-          <div className="set_score_button_score">15</div>
-          <div className="set_score_button_label">+1</div>
-        </button>
-        <button className="set_score_decrement">
-          <div className="set_score_decrement_label">&minus;1</div>
-        </button>
-        <button className="set_score_timeout timeout_1">15-10</button>
-        <button className="set_score_timeout timeout_2">T-O</button>
-      </div>
-      <div className="set_match_score">1</div>
+    <TeamSet side="left" teamName="ASV 7" score="15" />
+    <TeamSet side="right" teamName="Viborg" score="10" />
 
-      <div className="set_substitutions">
-        <div className="set_substitutions_title">Udskiftninger</div>
-        <div className="set_substitutions_cell">
-          <div className="set_substitutions_cell_in">1</div>
-          <div className="set_substitutions_cell_in_label">Ind</div>
-          <div className="set_substitutions_cell_out">23</div>
-          <div className="set_substitutions_cell_out_label">Ud</div>
-        </div>
-        <div className="set_substitutions_cell">
-          <button className="set_substitutions_cell_add">+</button>
-        </div>
-        <div className="set_substitutions_cell"></div>
-        <div className="set_substitutions_cell"></div>
-        <div className="set_substitutions_cell"></div>
-        <div className="set_substitutions_cell"></div>
-      </div>
-
-      <div className="set_lineup">
-        <div className="set_lineup_title">Opstilling</div>
-        <div className="set_lineup_cell">23</div>
-        <div className="set_lineup_cell">12</div>
-        <div className="set_lineup_cell">8</div>
-        <div className="set_lineup_cell">7</div>
-        <div className="set_lineup_cell">2</div>
-        <div className="set_lineup_cell">1</div>
-      </div>
-    </div>
-
-    <div className="set_team team_right">
-      <div className="set_score">
-        <div className="set_team_name">Viborg</div>
-        <button className="set_score_button">
-          <div className="set_score_button_score">10</div>
-          <div className="set_score_button_label">+1</div>
-        </button>
-        <button className="set_score_decrement">
-          <div className="set_score_decrement_label">&minus;1</div>
-        </button>
-        <button className="set_score_timeout timeout_1">T-O</button>
-        <button className="set_score_timeout timeout_2">T-O</button>
-      </div>
-      <div className="set_match_score">1</div>
-      <div className="set_substitutions">
-        <div className="set_substitutions_title">Udskiftninger</div>
-        <div className="set_substitutions_cell">
-          <div className="set_substitutions_cell_in">23</div>
-          <div className="set_substitutions_cell_in_label">Ind</div>
-          <div className="set_substitutions_cell_out">12</div>
-          <div className="set_substitutions_cell_out_label">Ud</div>
-        </div>
-        <div className="set_substitutions_cell">
-          <button className="set_substitutions_cell_add">+</button>
-        </div>
-        <div className="set_substitutions_cell"></div>
-        <div className="set_substitutions_cell"></div>
-        <div className="set_substitutions_cell"></div>
-        <div className="set_substitutions_cell"></div>
-      </div>
-      <div className="set_lineup">
-        <div className="set_lineup_title">Opstilling</div>
-        <div className="set_lineup_cell">23</div>
-        <div className="set_lineup_cell">12</div>
-        <div className="set_lineup_cell">8</div>
-        <div className="set_lineup_cell">7</div>
-        <div className="set_lineup_cell">2</div>
-        <div className="set_lineup_cell">1</div>
-      </div>
-    </div>
     </div>
 
     <div className="modal">
@@ -150,6 +70,55 @@ var MatchForm = React.createClass({
       </div>
 
     </div>
+    </div>
+    );
+  }
+});
+
+var TeamSet = React.createClass({
+  render: function () {
+    return (
+    <div className={"set_team team_"+this.props.side}>
+      <div className="set_score">
+        <div className="set_team_name">{this.props.teamName}</div>
+        <button className="set_score_button">
+          <div className="set_score_button_score">{this.props.score}</div>
+          <div className="set_score_button_label">+1</div>
+        </button>
+        <button className="set_score_decrement">
+          <div className="set_score_decrement_label">&minus;1</div>
+        </button>
+        <button className="set_score_timeout timeout_1">15-10</button>
+        <button className="set_score_timeout timeout_2">T-O</button>
+      </div>
+      <div className="set_match_score">1</div>
+
+      <div className="set_substitutions">
+        <div className="set_substitutions_title">Udskiftninger</div>
+        <div className="set_substitutions_cell">
+          <div className="set_substitutions_cell_in">1</div>
+          <div className="set_substitutions_cell_in_label">Ind</div>
+          <div className="set_substitutions_cell_out">23</div>
+          <div className="set_substitutions_cell_out_label">Ud</div>
+        </div>
+        <div className="set_substitutions_cell">
+          <button className="set_substitutions_cell_add">+</button>
+        </div>
+        <div className="set_substitutions_cell"></div>
+        <div className="set_substitutions_cell"></div>
+        <div className="set_substitutions_cell"></div>
+        <div className="set_substitutions_cell"></div>
+      </div>
+
+      <div className="set_lineup">
+        <div className="set_lineup_title">Opstilling</div>
+        <div className="set_lineup_cell">23</div>
+        <div className="set_lineup_cell">12</div>
+        <div className="set_lineup_cell">8</div>
+        <div className="set_lineup_cell">7</div>
+        <div className="set_lineup_cell">2</div>
+        <div className="set_lineup_cell">1</div>
+      </div>
     </div>
     );
   }
